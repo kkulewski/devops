f39a83bbe1012f03319bf4d9cd2b7642217f77a2
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2020-04-25 15:57:23 +0200

    Repo cleanup

---
+++ backend/make-service.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+docker stop my-backend my-redis my-postgres
+docker network rm my-network
+docker rmi backend
+
+docker network create my-network
+docker build -f Dockerfile.dev . -t backend
+docker run -d --rm --network my-network --name my-redis redis
+docker run -d --rm --network my-network --name my-postgres -e POSTGRES_PASSWORD=1234 postgres
+docker run -d --rm --network my-network --name my-backend -p 5000:5000 -e REDIS_HOST=my-redis -e REDIS_PORT=6379 -e PGHOST=my-postgres -e PGPORT=5432 -e PGUSER=postgres -e PGPASSWORD=1234 -e PGDATABASE=postgres -v /opt/app/node_modules -v $(pwd):/opt/app backend
--- multicontainerdemo/backend/keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    redisHost: process.env.REDIS_HOST,
-    redisPort: process.env.REDIS_PORT,
-    pgHost: process.env.PGHOST,
-    pgPort: process.env.PGPORT,
-    pgUser: process.env.PGUSER,
-    pgPassword: process.env.PGPASSWORD,
-    pgDatabase: process.env.PGDATABASE,
-}
